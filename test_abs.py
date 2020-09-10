b5cc055651ede1751f4e33f851c792fcfa3cf5d9
grecha505@gmail.com <droppot_1342>
2020-09-10 23:52:50 +0300

    reorganised project + new homework

---
+++ test_abs.py
@@ -0,0 +1,9 @@
+# PyTest learning
+
+
+def test_abs1():
+    assert abs(-42) == 42, "Should ba an absolute vlaue of a number"
+
+
+def test_abs2():
+    assert abs(-42) == -42, "Should ba an absolute vlaue of a number"
